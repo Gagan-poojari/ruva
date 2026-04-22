faa58398ec171890eebca0227e93e4d3112929e2
Gagan Poojari <gagancgp@gmail.com>
2026-04-22 19:22:39 +0530

    backend deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 
 // Middleware
 app.use(cors(
-    
+    {
+        origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://ruva.vercel.app' || 'https://localhost:3001',
+        credentials: true,
+    }
 ));
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
